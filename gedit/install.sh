04954d704f183b4256f804d5a37c3e5b1f357af3
Mazhar Hussain <realmazharhussain@gmail.com>
2021-02-06 00:52:47 +0500

    Added smart GEdit (ge and sgedit)

--- gedit/install.sh
+++ gedit/install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 sdir=$(realpath $(dirname $0))
+binDir="$root"/usr/local/bin
+appsDir="$root"/usr/share/applications
+gtk4styleDir="$root"/usr/share/gtksourceview-4/styles
+gtk3styleDir="$root"/usr/share/gtksourceview-3.0/styles
+
 if [[ $UID = '0' ]]; then
   echo 'installing gEdit files ...'
-  mkdir -p "$root"/usr/share/gtksourceview-4/styles/ "$root"/usr/share/gtksourceview-3.0/styles/
-  cp "$sdir"/color-schemes/*.xml "$root"/usr/share/gtksourceview-4/styles/
-  cp "$sdir"/color-schemes/*.xml "$root"/usr/share/gtksourceview-3.0/styles/
+  mkdir -p  "$gtk3styleDir" "$gtk4styleDir" "$appsDir" "$binDir"
+  cp "$sdir"/color-schemes/*.xml "$gtk3styleDir"/
+  cp "$sdir"/color-schemes/*.xml "$gtk4styleDir"/
+  chmod +x "$sdir"/bin/*
+  cp "$sdir"/bin/* "$binDir"/
+  cp "$sdir"/applications/*.desktop "$appsDir"/
 else
   sudo --preserve-env=root $0
 fi
