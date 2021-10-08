8bce4f95a0a186d7db2e49b6a272f2fffe65a91b
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-10-08 18:44:55 +0500

    generic/bin/new: installable through pacman now

--- generic/install-system.sh
+++ generic/install-system.sh
@@ -6,11 +6,11 @@ if [[ $UID = '0' ]]; then
   [ -z "$PREFIX" ] && PREFIX=/usr/local
 
   echo 'installing system-wide files ...'
-  mkdir -p "$DESTDIR"/{$PREFIX/share/applications,$PREFIX/bin,etc/xdg/autostart}
-  cp "$sdir"/applications/*.desktop "$DESTDIR"/$PREFIX/share/applications/
-#  cp "$sdir"/autostart/*.desktop "$DESTDIR"/etc/xdg/autostart/
-  chmod +x "$sdir"/bin/*
-  cp "$sdir"/bin/* "$DESTDIR"/$PREFIX/bin/
+  mkdir -p "$DESTDIR/$PREFIX"/{bin,share/{applications,fish/vendor_completions.d}}
+  install -m644 "$sdir"/applications/*.desktop "$DESTDIR"/$PREFIX/share/applications/
+#  install -Dm644 "$sdir"/autostart/*.desktop "$DESTDIR"/etc/xdg/autostart/
+  install -m755 "$sdir"/bin/* "$DESTDIR"/$PREFIX/bin/
+  install -m755 "$sdir"/completions/* "$DESTDIR/$PREFIX"/share/fish/vendor_completions.d/
 else
   sudo --preserve-env=DESTDIR,PREFIX "$0"
 fi
