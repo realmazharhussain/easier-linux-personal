fe9d56ed3acd10caa6a20a76cff42c5813444e61
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2022-03-11 19:50:02 +0500

    fix completions/new.fish

--- generic/completions/new.fish
+++ generic/completions/new.fish
@@ -1,4 +1,6 @@
-set -l __templates (ls -1 ~/Templates)
+set -l __templates (ls -1 ~/Templates | string replace -r '\..*$' '')
+set -l templates
 for template in $__templates
-  complete -x -c new -a \"(echo $template | string replace -r '\..*$' '')\" -d "Template"
+  set -a templates \'$template\'
 end
+complete -x -c new -n "not __fish_seen_subcommand_from $templates" -a "$templates" -d "Template"
